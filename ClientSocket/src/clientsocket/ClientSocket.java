adc44ec20f5bc69687b24e97255517ad7763ebaa
ricxpg <puggioni.riccardo@studenti.salesianisesto.it>
2022-02-16 09:57:27 +0100

    edit client

--- ClientSocket/src/clientsocket/ClientSocket.java
+++ ClientSocket/src/clientsocket/ClientSocket.java
@@ -9,6 +9,7 @@ import java.net.*;
 import java.io.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.*;
 
 /**
  *
@@ -19,17 +20,33 @@ public class ClientSocket {
     /**
      * @param args the command line arguments
      */
+    public static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
+        String a;
+        boolean b = true;
         try {
-            Socket clientSocket = new Socket("10.1.33.200", 5000);
+            Socket clientSocket = new Socket("10.1.33.19", 5000);
             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            out.println("hello server");
+            do{
+            System.out.println("dì qualcosa al server:");
+            a = sc.next();
+            out.println(a);
             String resp = in.readLine();
-            System.out.println("risposta del server: " + resp);
-            in.close();
-            out.close();
-            clientSocket.close();
+            if (a == "stop") {
+                System.out.println("addio");
+                in.close();
+                out.close();
+                clientSocket.close();
+                b = false;
+            } else {
+                a.toUpperCase();
+                resp = a;
+                System.out.println("risposta del server: " + resp);
+            }
+            }while(b = true);
+
         } catch (IOException ex) {
             Logger.getLogger(ClientSocket.class.getName()).log(Level.SEVERE, null, ex);
         }
